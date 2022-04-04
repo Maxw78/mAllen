bf3681bf15e1aa6412e5ec16c7565c3219ace22c
Maxw78 <maxwallen78@gmail.com>
2022-04-04 23:00:15 +0000

    Admin access given, Migrations made, classes updated

--- WebShop/WebShopApp/admin.py
+++ WebShop/WebShopApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Product
+from .models import Profile
+from .models import Review
 # Register your models here.
+
+@admin.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'brand', 'average_cost', 'product_category', 'release_date', 'description'] 
+
+@admin.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'date_of_birth', 'address', 'city', 'country', 'user_photo']
+
+@admin.register(Review)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['product_rating', 'review', 'date']
--- WebShop/WebShopApp/models.py
+++ WebShop/WebShopApp/models.py
@@ -1,25 +1,27 @@
 from sqlite3 import Date
+from unicodedata import name
 from django.db import models
 from datetime import datetime
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    product_name = models.CharField(max_length=100, blank=False)
-    brand = models.CharField(max_length=100, blank=False)
+    product_name = models.CharField(max_length=40, blank=False)
+    brand = models.CharField(max_length=30, blank=False)
     average_cost = models.IntegerField(default=0, blank=False)
     CATEGORY_CHOICES = [('TV', 'TV'), ('Tablet', 'Tablet'), ('Phone', 'Phone')]
     product_category = models.CharField(max_length=15, choices=CATEGORY_CHOICES)
-    release_date = models.DateField(default=Date, blank=False)
-    description = models.CharField(max_length=100, blank=False)
+    release_date = models.DateField()
+    description = models.TextField(max_length=100, blank=False)
 
 def _str_ (self):
-    return self.Product
+    return self.product_name
 
 class Profile(models.Model):
     profile_id = models.AutoField(primary_key=True, blank=False)
     full_name = models.CharField(max_length=30, blank=False)
-    date_of_birth = models.DateField(default=Date, blank=False)
+    date_of_birth = models.DateField()
     address = models.CharField(max_length=50, blank=False)
     city = models.CharField(max_length=50, blank=False)
     COUNTRY_CHOICES = [('UK', 'UK'), ('France', 'France'), ('Germany', 'Germany'), ('Spain', 'Spain')]
@@ -27,13 +29,13 @@ class Profile(models.Model):
     user_photo = models.ImageField(upload_to = 'user_photo/')
 
 def _str_ (self):
-    return self.Profile
+    return self.full_name
 
 class Review(models.Model):
     review_id = models.AutoField(primary_key=True, blank=False)
-    product_rating = models.IntegerField(default=0, blank=False)
-    review = models.CharField(max_length=30, blank=False)
-    date = models.DateField(default=Date, blank=False)
+    product_rating = models.IntegerField(default=0, validators=[MaxValueValidator(5)], blank=False)
+    review = models.TextField(max_length=255, blank=False)
+    date = models.DateField()
 
 def _str_ (self):
-    return self.Review
+    return self.product_rating
