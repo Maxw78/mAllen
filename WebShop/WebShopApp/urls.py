42d6c473019f87657088f8c7334d56dc103ba866
Maxw78 <maxwallen78@gmail.com>
2022-04-05 03:52:46 +0000

    Registration and log in and out function completed, basic profile function completed.

--- WebShop/WebShopApp/urls.py
+++ WebShop/WebShopApp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', views.home, name='WebShop'),
     path('about/', views.AboutUs, name='WebShop-About'),
     path('contactus/', views.ContactUs, name='WebShop-Contact'),
     path('products/', views.Products, name='WebShop-Products'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    
 ]
--- WebShop/WebShopApp/views.py
+++ WebShop/WebShopApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+
 def home(request):
     return render(request, 'home.html', {'title': 'Home'})
 
@@ -12,3 +18,21 @@ def ContactUs(request):
 
 def Products(request):
     return render(request, 'products.html', {'title': 'Products'})
+
+def register(request):
+
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+        else:
+            form = UserRegisterForm()
+            messages.success(request, f'Account not created yet')
+        return render(request, 'register.html', {'form':form})
+        
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
